6455007232e9592a99e1fe3ba05b9698243ad718
Alex <4reg4444@gmail.com>
2019-03-09 01:23:39 +0300

    add hero

--- components/Header.vue
+++ components/Header.vue
@@ -1,48 +1,68 @@
 <template>
-  <nav class="navbar">
-    <div class="navbar-brand">
-      <nuxt-link active-class="navbar-item" to="/">
-        <span style="color: red">BEAUTY</span>
-        <span style="color: blue">AFISHA</span>
-      </nuxt-link>
-      <a
-        :class="{ 'is-active': burgerIsActive }"
-        role="button"
-        class="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-        @click="toogle"
-      >
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-      </a>
-    </div>
-    <div :class="{ 'is-active': burgerIsActive }" class="navbar-menu">
-      <div class="navbar-start">
-        <nuxt-link class="navbar-item" to="/">
-          Главная
-        </nuxt-link>
-        <nuxt-link class="navbar-item" to="/events">
-          Мероприятия
-        </nuxt-link>
-        <nuxt-link class="navbar-item" to="/vacancies">
-          Вакансии
-        </nuxt-link>
-        <nuxt-link class="navbar-item" to="/schools">
-          Куда пойти учиться
-        </nuxt-link>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          Выберите город:
-          <Filters />
+  <section class="hero is-primary">
+    <div class="hero-head">
+      <nav class="navbar is-size-4">
+        <div class="navbar-brand">
+          <nuxt-link active-class="navbar-item" to="/">
+            <span style="color: red">BEAUTY</span>
+            <span style="color: blue">AFISHA</span>
+          </nuxt-link>
+          <a
+            :class="{ 'is-active': burgerIsActive }"
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            @click="toogle"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
         </div>
+        <div :class="{ 'is-active': burgerIsActive }" class="navbar-menu">
+          <div class="navbar-start">
+            <nuxt-link class="navbar-item" to="/">
+              Главная
+            </nuxt-link>
+            <nuxt-link class="navbar-item" to="/events">
+              Мероприятия
+            </nuxt-link>
+            <nuxt-link class="navbar-item" to="/vacancies">
+              Вакансии
+            </nuxt-link>
+            <nuxt-link class="navbar-item" to="/schools">
+              Куда пойти учиться
+            </nuxt-link>
+          </div>
+          <div class="navbar-end">
+            <div class="navbar-item is-size-6">
+              <span>Ваш город:&nbsp;</span><Filters />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          Beauty Afisha
+        </h1>
+        <h2 class="subtitle">
+          Путеводитель для профессионалов в сфере красоты
+        </h2>
       </div>
     </div>
-  </nav>
+  </section>
 </template>
+
+<style>
+.hero {
+  margin-bottom: 30px;
+}
+</style>
+
 <script>
 import Filters from '~/components/filters/changeCity.vue'
 
