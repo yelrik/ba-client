2c9e1b66bca56ff288c1a93f7ded731c96c9b85f
Alex <4reg4444@gmail.com>
2019-03-09 01:27:13 +0300

    add last 3 events in view

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="container">
-    Здесь будет контент
-  </div>
+  <section class="section hero">
+    <div class="hero-body">
+      <div v-for="event in last3Events" :key="event.id" class="container">
+        <div class="columns">
+          <div class="column">
+            <p class="title is-2 has-text-centered">
+              {{ event.title }}
+            </p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="title is-4">
+              {{ event.start_datetime }}
+            </p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <img :src="event.image.data.full_url" alt="">
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="title is-5">
+              {{ event.place.adress }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      last3Events: []
+    }
+  },
+  computed: {
+    cityId: function() {
+      return this.$store.getters.cityChangedId
+    }
+  },
+  watch: {
+    cityId: function() {
+      this.getList()
+    }
+  },
+  mounted: async function() {
+    const response = await this.$axios.$get(
+      'http://62.109.31.76/test.php?id=' + this.$store.getters.cityChangedId
+    )
+    this.last3Events = response
+  },
+  methods: {
+    async getList() {
+      const response = await this.$axios.$get(
+        'http://62.109.31.76/test.php?id=' + this.cityId
+      )
+      this.last3Events = response
+    }
+  }
+}
+</script>
