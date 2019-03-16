7a618b1d2d94ddd0c66af4baa4da502bef132522
Alex <4reg4444@gmail.com>
2019-03-16 23:48:42 +0300

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="footer content">
+    <div class="columns">
+      <div class="column has-text-centered">
+        <span class="has-text-weight-semibold">По вопросам сотрудничества:</span> <span class="has-text-link">info@beauty-afisha.ru</span>
+      </div>
+    </div>
+    <div class="columns is-centered is-hidden-desktop">
+      <div class="column is-4" />
+      <div class="column has-text-centered">
+        <p class="has-text-weight-semibold">
+          Пишите нам: 
+        </p>
+        <img src="~/assets/telegram_icon.svg" alt="">
+        <img src="~/assets/whatsapp_icon.svg" alt="">
+      </div>
+      <div class="column is-4" />
+    </div>
+    <div class="columns is-vcentered">
+      <div class="column is-4" />
+      <div class="column has-text-centered is-vcentered">
+        <p class="has-text-weight-semibold">
+          Подписывайтесь на нас: 
+        </p>
+        <img src="~/assets/vk_icon.svg" alt="">
+        <img src="~/assets/instagram_icon.svg" alt="">
+      </div>
+      <div class="column is-4" />
+    </div>
+    <div class="columns">
+      <div class="column has-text-centered">
+        <span class="has-text-grey-light">Beauty Afisha. Копирование материалов без письменного разрешения - запрещено</span>
+      </div>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div class="container">
     <Header />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
